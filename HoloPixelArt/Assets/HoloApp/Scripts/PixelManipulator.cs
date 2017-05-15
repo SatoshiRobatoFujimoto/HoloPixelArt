93b56c6c3ac2bc20fb7fe30ad063d141817cc6bb
Thomas Masquart <thomas.masquart@absolute.de>
2017-05-15 14:55:10 +0200

    Finish the Pixel manipulator, add a click sound to play when the Pixel is clicked

--- HoloPixelArt/Assets/HoloApp/Scripts/PixelManipulator.cs
+++ HoloPixelArt/Assets/HoloApp/Scripts/PixelManipulator.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PixelManipulator : MonoBehaviour {
+namespace HoloPixelArt
+{
+    public class PixelManipulator : MonoBehaviour, IInputClickHandler
+    {
+        public int Id;
+        public AudioClip Click;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private AudioSource _audioSource;
+        private Renderer _renderer;
+        private Color _color;
+
+        // Use this for initialization
+        void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _renderer = GetComponent<Renderer>();
+            _color = Color.blue;
+        }
+
+        public void ChangeColor(Color color)
+        {
+            _color = color;
+        }
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            if(Click != null)
+            {
+                _audioSource.PlayOneShot(Click);
+            }
+            _renderer.material.color = _color; 
+        }
+    }
 }
